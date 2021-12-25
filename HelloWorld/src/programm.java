0da569d2ca9d13326a75fe8e835ef22f8b04b20c
fkhonjoosh <fkhonjoosh@gmail.com>
2021-12-25 17:29:47 +0100

    second commit

--- HelloWorld/src/programm.java
+++ HelloWorld/src/programm.java
@@ -3,8 +3,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 		for (int i=1; i<=10; i++) {
-        // first change 
-		System.out.println("Hallo World ..."+i);	
+        	
 		System.out.println("Hallo World ..."+i);
 
 		}
