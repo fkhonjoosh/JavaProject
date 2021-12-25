1f43d48f35ed99227dd97d25b1aa2843ac0cf33b
fkhonjoosh <fkhonjoosh@gmail.com>
2021-12-25 17:19:40 +0100

    First commit

---
+++ HelloWorld/src/programm.java
@@ -0,0 +1,12 @@
+public class Program {
+
+	public static void main(String[] args) {
+		
+		for (int i=1; i<=10; i++) {
+        // first change 
+		System.out.println("Hallo World ..."+i);	
+		System.out.println("Hallo World ..."+i);
+
+		}
+		
+	}
