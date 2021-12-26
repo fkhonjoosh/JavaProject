bb0d3049734219bb042918c2b4996b8727f737f0
fkhonjoosh <fkhonjoosh@gmail.com>
2021-12-26 11:09:48 +0100

    second commit in Branch FeatureA

--- HelloWorld/src/programm.java
+++ HelloWorld/src/programm.java
@@ -4,9 +4,11 @@ public class Program {
 		
 		for (int i=1; i<=10; i++) {
         // Branche featureA
-        System.out.println("Hallo World ...");	
+        System.out.println("Hallo World ...");
+        // Branche featureA next commit
 		System.out.println("Hallo World ..."+i);
-
+        // New comment 
+        System.out.println("Hallo FeatureA ..."+i);
 		}
 		
 	}
